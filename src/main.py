eaccc001e71570f4ee9ccabc5a500bc8dd8063c6
Sl3nc <pedrohrm050315@hotmail.com>
2025-06-16 14:03:44 -0300

    window kv

--- src/main.py
+++ src/main.py
@@ -9,31 +9,30 @@ from kivy.app import App
 from kivy.uix.widget import  Widget
 from kivy.lang import Builder
 
-# Builder.load_file('window.kv')
+Builder.load_file('window.kv')
 
 class Downloader(BoxLayout):
+    pass
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.orientation = 'vertical'
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = 'vertical'
+    #     self.add_widget(Label(
+    #         text='Para baixar automaticamente seus recibos da DCTF WEB;\n'\
+    #         'Siga as seguintes instrunções:',
+    #         valign='top',
+    #         halign='center'
+    #     ))
 
-        self.add_widget(Label(
-            text='Para baixar automaticamente seus recibos da DCTF WEB;\n'\
-            'Siga as seguintes instrunções:',
-            valign='top',
-            halign='center'
-        ))
+    #     btns_box = BoxLayout(padding=20)
+    #     btns_box.add_widget(Button(text='Enviar'))
 
+    #     btns_utils_box = BoxLayout(orientation='vertical')
+    #     btns_utils_box.add_widget(Button(text='Voltar'))
+    #     btns_utils_box.add_widget(Button(text='Pular'))
 
-        btns_box = BoxLayout(padding=20)
-        btns_box.add_widget(Button(text='Enviar'))
-
-        btns_utils_box = BoxLayout(orientation='vertical')
-        btns_utils_box.add_widget(Button(text='Voltar'))
-        btns_utils_box.add_widget(Button(text='Pular'))
-
-        btns_box.add_widget(btns_utils_box)
-        self.add_widget(btns_box)
+    #     btns_box.add_widget(btns_utils_box)
+    #     self.add_widget(btns_box)
 
 class DownloaderApp(App):
     def build(self):
