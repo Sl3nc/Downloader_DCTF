961d764cf84176a99376bba142dd8c1ac5ccd48e
Sl3nc <pedrohrm050315@hotmail.com>
2025-06-17 12:00:48 -0300

    step

--- src/main.py
+++ src/main.py
@@ -1,38 +1,61 @@
-from kivy.uix.rst import nodeclass
-from kivy.uix.actionbar import Button
-from kivy.uix.vkeyboard import Canvas
-from kivy.uix.gesturesurface import Line
-from kivy.uix.actionbar import Label
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.actionbar import BoxLayout
-from kivy.app import App
-from kivy.uix.widget import  Widget
 from kivy.lang import Builder
+from kivy.app import App
 
 Builder.load_file('window.kv')
 
+class Step:
+    value = 0
+    message_index = 0
+    func_index = 1
+    browser = ...
+    instructions = [
+        ['Primeiramente, clique em "iniciar"', None],
+        #Abre o navegador no passo 1
+        ['Um navegador foi aberto na página do ECAC, certo?\nFaça login na conta da Deltaprice (clique em prosseguir quando acabar)', None]
+    ]
+
+    def __init__(self):
+        pass
+
+    def next(self) -> str:
+        self.value = self.value + 1
+        if self.value == len(self.instructions): self.value = 0
+        data = self.instructions[self.value]
+
+        func  = data[self.func_index]
+        if func != None: func()
+
+        return data[self.message_index]
+
+    def back(self) -> str:
+        self.value = self.value - 1
+        return self.instructions[self.value][self.message_index]
+
+    def jump(self) -> str:
+        self.value = len(self.instructions) - 1
+        return self.instructions[self.value][self.message_index]
+
 class Downloader(BoxLayout):
+    step = Step()
     pass
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-    #     self.orientation = 'vertical'
 
-    #     self.add_widget(Label(
-    #         text='Para baixar automaticamente seus recibos da DCTF WEB;\n'\
-    #         'Siga as seguintes instrunções:',
-    #         valign='top',
-    #         halign='center'
-    #     ))
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ref = {
+            0: lambda: self.step.next(),
+            1: lambda: self.step.back(),
+            2: lambda: self.step.jump()
+        }
 
-    #     btns_box = BoxLayout(padding=20)
-    #     btns_box.add_widget(Button(text='Enviar'))
+    #Executado pelo enviar / prosseguir
+    def send(self, value = 0):
+        result = self.ref[value]
+        print(result())
+        
+        #Receber texto do botão também
+        #Define o result como valor da instrunção
 
-    #     btns_utils_box = BoxLayout(orientation='vertical')
-    #     btns_utils_box.add_widget(Button(text='Voltar'))
-    #     btns_utils_box.add_widget(Button(text='Pular'))
-
-    #     btns_box.add_widget(btns_utils_box)
-    #     self.add_widget(btns_box)
 
 class DownloaderApp(App):
     def build(self):
